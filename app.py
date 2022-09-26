3bb67fde0f02165d727cb61d7d85e281fd864091
Pragya Khatri <pragya.kaurava@gmail.com>
2022-09-26 17:00:17 +0000

    corrected error

--- app.py
+++ app.py
@@ -4,28 +4,28 @@ app = Flask(__name__)
 
 JOBS = [
   {
-    'id' = 1,
-    'title' = 'Data Analyst'
-    'location' = 'Bangaluru, India'
-    'salary' = 'Rs. 10,00,000'
+    'id' : 1,
+    'title' : 'Data Analyst'
+    'location' : 'Bangaluru, India'
+    'salary' : 'Rs. 10,00,000'
   },
   {
-    'id' = 2,
-    'title' = 'Data Scientist'
-    'location' = 'Delhi, India'
-    'salary' = 'Rs. 15,00,000'
+    'id' : 2,
+    'title' : 'Data Scientist'
+    'location' : 'Delhi, India'
+    'salary' : 'Rs. 15,00,000'
   },
   {
-    'id' = 3,
-    'title' = 'Front End Engineer'
-    'location' = 'Remote'
-    'salary' = 'Rs. 12,00,000'
+    'id' : 3,
+    'title' : 'Front End Engineer'
+    'location' : 'Remote'
+    'salary' : 'Rs. 12,00,000'
   },
   {
-    'id' = 4,
-    'title' = 'Back End Engineer'
-    'location' = 'San Francisco, USA'
-    'salary' = '$120,000'
+    'id' : 4,
+    'title' : 'Back End Engineer'
+    'location' : 'San Francisco, USA'
+    'salary' : '$120,000'
   }
 ]
 
