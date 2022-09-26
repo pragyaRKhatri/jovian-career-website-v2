04a7d12260482ef5e1e96633cb864f0c1cdc13ff
Pragya Khatri <pragya.kaurava@gmail.com>
2022-09-26 17:33:32 +0000

    commiting changes

--- app.py
+++ app.py
@@ -4,27 +4,27 @@ app = Flask(__name__)
 
 JOBS = [
   {
-    'id' : 1,
-    'title' : 'Data Analyst'
-    'location' : 'Bangaluru, India'
+    'id': 1,
+    'title': 'Data Analyst',
+    'location': 'Bangaluru, India',
     'salary' : 'Rs. 10,00,000'
   },
   {
     'id' : 2,
-    'title' : 'Data Scientist'
-    'location' : 'Delhi, India'
+    'title' : 'Data Scientist',
+    'location' : 'Delhi, India',
     'salary' : 'Rs. 15,00,000'
   },
   {
     'id' : 3,
-    'title' : 'Front End Engineer'
-    'location' : 'Remote'
+    'title' : 'Front End Engineer',
+    'location' : 'Remote',
     'salary' : 'Rs. 12,00,000'
   },
   {
     'id' : 4,
-    'title' : 'Back End Engineer'
-    'location' : 'San Francisco, USA'
+    'title' : 'Back End Engineer',
+    'location' : 'San Francisco, USA',
     'salary' : '$120,000'
   }
 ]
